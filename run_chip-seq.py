2ac6219bed85db9cb3dd6704e3cafc99cda05d23
calizarr <calizarr@gmail.com>
2015-11-16 14:13:24 -0600

    Updating scripts to run with wrapper script.

--- run_chip-seq.py
+++ run_chip-seq.py
@@ -16,12 +16,14 @@ def options():
     parser.add_argument("-map_reads", action="store_true", help="Map reads to reference")
     parser.add_argument("-sicer_rb", action="store_true", help="Run SICER without controls")
     parser.add_argument("-sicer", action="store_true", help="Run SICER with controls")
+    parser.add_argument("-bed", action="store_true", help="Convert bams to beds for SICER")
     parser.add_argument("-config", default = None, help="Path to config file")
     parser.add_argument("-outdir", help="Path to output directory (log files")
     args = parser.parse_args()
     return args
 
 def main():
+    # Getting the command line options and reading config.
     args = options()
     if args.config:
         if sys.version_info[0] < 3:
@@ -31,7 +33,9 @@ def main():
             import configparser
             Config = configparser.ConfigParser()
         Config.read(args.config)
+    # List of commands to be printed at end.
     cmdlist = []
+    # Getting the current date and setting log files.
     now = datetime.datetime.now().strftime("%Y-%m-%d")
     log_out = "{0}_ChIP-seq.out.log".format(now)
     log_err = "{0}_ChIP-seq.err.log".format(now)
@@ -44,7 +48,7 @@ def main():
         files = glob.glob(os.path.join(input_dir,"*.fastq"))
         # files = Config.get("TRIM", "input_file").split(',')
         # files = [f.strip() for f in files]
-        for f in files:
+        for f in files: 
             cmd = "python trim_adapters.py -input_file {0} -config {1}".format(f, args.config)
             # cmd = logcommand.format(insert=cmd)
             cmdlist.append(cmd)
@@ -63,16 +67,53 @@ def main():
             cmd  = "python map_reads.py -input_file {0} -config {1}".format(f, args.config)
             # cmd = logcommand.format(insert=cmd)
             cmdlist.append(cmd)
-    if args.sicer_rb or args.sicer:
+    if args.bed:
+        # Converting BAM files to Bed files to Chromosome numbered Bed files for SICER
+        print("echo Converting BAM files to Bed files to Chromosome numbered Bed files for SICER")
         bam_dir = os.path.join(Config.get("ALL", "map_dir"), "STAR")
         files = glob.glob(os.path.join(bam_dir,"*/*Aligned.out.bam"))
-        
+        output_base = os.path.join(bam_dir, "Bed")
+        print("echo Attempting to create directory base: {0}".format(output_base))
+        try:
+            os.makedirs(output_base)
+        except OSError:
+            if not os.path.isdir(output_base):
+                raise
+        bam2bed = {'bam_dir': bam_dir, 'outdir': output_base}
+        cmd = "for x in {bam_dir}/*.Aligned.out.bam;do y=$(basename ${{x%.*}});bam2bed < $x > {outdir}/$y.bed & done;wait;echo Converting to Bed done".format(**bam2bed)
+        cmdlist.append(cmd)
+        output_bed = os.path.join(output_base, "Bed_chr")
+        print("echo Attempting to create directory base: {0}".format(output_bed))
+        try:
+            os.makedirs(output_bed)
+        except OSError:
+            if not os.path.isdir(output_base):
+                raise
+        bed2bed = {'bed_dir': output_base, 'original': "Bd"}
+        cmd = "for x in {bed_dir}/*.bed;do y=$(basename ${{x%.*}});sed -- 's/{original}/chr/g' $x > $y.sicer.bed & done;wait;echo Converting to Bed chromosome done".format(**bed2bed)
+        cmdlist.append(cmd)
     if args.sicer_rb:
         # run_sicer-rb.py InputDir bed_file OutputDir --species --rdthresh --winsize --fragsize --egf --gap_size --eval --sicer
         # More info available by running python run_sicer-rb.py -h
-        cmd = "python run_sicer-rb.py -config {0}".format(args.config)
-        # cmd = logcommand.format(insert=cmd)
-        cmdlist.append(cmd)
+        bed_dir = Config.get("ALL", "bed_dir")
+        files = (glob.glob(os.path.join(bed_dir,"*.sicer.bed")))
+        for f in files:
+            dic = {"input":f, "conf":args.config}
+            cmd = "python run_sicer-rb.py -input_file {input} -config {conf}".format(**dic)
+            # cmd = logcommand.format(insert=cmd)
+            cmdlist.append(cmd)
+    if args.sicer:
+        # run_sicer.py InputDir bed_file control_file OutputDir --species --rdthresh --winsize --fragsize --egf --gap_size --FDR --sicer
+        # More info available by running python run_sicer.py -h
+        bed_dir = Config.get("ALL", "bed_chr_dir")
+        samples = glob.glob(os.path.join(bed_dir, "*sample*.sicer.bed"))
+        controls = glob.glob(os.path.join(bed_dir, "*control*.sicer.bed"))
+        # Assuming equal number of samples and controls properly labelled.
+        joined = zip(samples, controls)
+        for j in joined:
+            dic = {"sample":j[0], "control":j[1], "conf":args.config}
+            cmd = "python run_sicer.py -sample {sample} -control {control} -config {conf}".format(**dic)
+            cmdlist.append(cmd)
 
     print('\n'.join(cmdlist))
 
--- trim_adapters.py
+++ trim_adapters.py
@@ -3,16 +3,17 @@ from __future__ import print_function
 import argparse
 import subprocess
 import os
+import sys
 
 
 def options():
     parser = argparse.ArgumentParser(description="Trim ChiP/ATAC-seq reads",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("nThreads", help="Number of threads to use for Trimmomatic")
-    parser.add_argument("cutoff_qual", help="Quality for nucleotide cutoff from head and tail")
-    parser.add_argument("minlen", help="Minimun length of read before drop off")
-    parser.add_argument("phred", help="Phred scores of the reads")
-    parser.add_argument("input_file", help="Read file for adapter trimming and filtering")
+    parser.add_argument("-nThreads", help="Number of threads to use for Trimmomatic", default="8")
+    parser.add_argument("-cutoff_qual", help="Quality for nucleotide cutoff from head and tail", default="30")
+    parser.add_argument("-minlen", help="Minimun length of read before drop off", default="30")
+    parser.add_argument("-phred", help="Phred scores of the reads", default="33")
+    parser.add_argument("-input_file", help="Read file for adapter trimming and filtering")
     parser.add_argument("-fa", "--fasta_adapters",
                         default = "/nfs4shares/bioinfosw/installs_current/Trimmomatic-0.32/adapters/TruSeq3-SE.fa",
                         help="Adapters file for trimmming adapters")
@@ -37,13 +38,45 @@ def options():
 
 def main():
     args = options()
+    # Either using a Config file or straight command line parameters.
+    if args.config:
+        if sys.version_info[0] < 3:
+            import ConfigParser
+            Config = ConfigParser.ConfigParser()
+        else:
+            import configparser
+            Config = configparser.ConfigParser()
+        Config.read(args.config)
+        nThreads = Config.get("ALL", "nThreads")
+        phred = Config.get("TRIM", "phred")
+        input_file = args.input_file
+        fasta_adapters = Config.get("TRIM", "fasta_adapters")
+        cutoff_qual = Config.get("TRIM", "cutoff")
+        minlen = Config.get("TRIM", "minlen")
+        java_command = Config.get("JAVA", "java_command")
+        min_heap = Config.get("JAVA", "min_heap")
+        max_heap = Config.get("JAVA", "max_heap")
+        trimmomatic = Config.get("TRIM", "trimmomatic")
+    elif not args.input_file:
+        sys.exit("Provide input_file please -input_file [filepath] \nCheck script defaults as well( -h).")
+    else:
+        nThreads = args.nThreads
+        phred = args.phred
+        input_file = args.input_file
+        fasta_adapters = args.fasta_adapters
+        cutoff_qual = args.cutoff_qual
+        minlen = args.minlen
+        java_command = args.java_command
+        min_heap = args.min_heap
+        max_heap = args.max_heap
+        trimmomatic = args.trimmomatic
     # Getting the absolute path to the file if a relative path was given (../file.ext)
-    args.input_file = os.path.abspath(args.input_file)
+    input_file = os.path.abspath(input_file)
     # Getting just the directory name
-    dirname = os.path.dirname(args.input_file)
+    dirname = os.path.dirname(input_file)
     print("Dirname is: {0}".format(dirname))
     # Getting just the file name
-    basename = os.path.basename(args.input_file)
+    basename = os.path.basename(input_file)
     print("Basename is: {0}".format(basename))
     output_base = os.path.join(dirname, "Filtered")
     print("Attempting to create directory base: {0}".format(output_base))
@@ -54,13 +87,13 @@ def main():
             raise
     # Creating the output filename.
     output_file = os.path.join(output_base, basename + ".filtered")
-    call = "{0} -Xms8g -Xmx15g -XX:+UseG1GC -XX:+UseStringDeduplication -jar {1}".format(args.java_command, args.trimmomatic)
+    call = "{0} -Xms{1} -Xmx{2} -XX:+UseG1GC -XX:+UseStringDeduplication -jar {3}".format(java_command, min_heap, max_heap, trimmomatic)
     cmd = "{0} SE -threads {1} -phred{2} {3} {4} ILLUMINACLIP:{5}:2:30:10 LEADING:{6} TRAILING:{7} MINLEN:{8}" \
-          .format(call, args.nThreads, args.phred, args.input_file, output_file, args.fasta_adapters,
-                  args.cutoff_qual, args.cutoff_qual, args.minlen)
+          .format(call, nThreads, phred, input_file, output_file, fasta_adapters,
+                  cutoff_qual, cutoff_qual, minlen)
     print("Running Trimmommatic with this command:")
     print(cmd, "\n")
-    subprocess.call(cmd, shell=True)
+    # subprocess.call(cmd, shell=True)
 
 if __name__ == "__main__":
     main()
